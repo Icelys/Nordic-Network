493c1786a06f280b5e3aeac4d3273860c156ff18
Tropic <tropical.productions.scratch@gmail.com>
2015-12-07 14:24:05 +0100

    Update top-menu.php

--- top-menu.php
+++ top-menu.php
@@ -1,5 +1,5 @@
 <?php
-echo '<a href="index.html">Nordic Network</a>
+echo '<a href="index.php">Nordic Network</a>
 <a href="register.html">Sign up</a>
 <a href="login.html">Sign in</a>
 <a href="about.html">About</a>
